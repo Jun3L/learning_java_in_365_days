c51943fe256e21af75188c864eb7f0d7239030d0
Jun3L <junel041106@gmail.com>
2025-12-26 19:55:54 +0800

    factorial of a number but in while loop

--- src/day9.java
+++ src/day9.java
@@ -2,13 +2,14 @@ import java.util.Scanner;
 public class day9 {
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
-        System.out.println("Enter a number: ");
+        System.out.print("Enter a number: ");
         int num = s.nextInt();
         int fact = 1;
-
-        for(int i = 1; i <= num; i++){
-              fact = fact * i;
-        }
+        int i = 1;
+       while (i <= num) {
+         fact = fact * i;
+         i++;
+       }
         System.out.println("The factorial of " + num + " is " + fact);
         s.close();
     }
