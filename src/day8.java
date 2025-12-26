36c79b53c1fa838e1c1b45c0ce1e0e8392e9928d
Jun3L <junel041106@gmail.com>
2025-12-26 19:40:15 +0800

    print natural numbers

---
+++ src/day8.java
@@ -0,0 +1,8 @@
+public class day8 {
+    public static void main(String[] args) {
+        System.out.println("Natural numbers: ");
+        for(int i = 0; i <= 20; i++){
+         System.out.println(i);
+        }
+    }
+}
