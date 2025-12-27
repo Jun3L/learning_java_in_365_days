52ffdc9bbd76827014a11f0789bfe2956f7fe946
Jun3L <junel041106@gmail.com>
2025-12-28 00:47:08 +0800

    Create a Person class with name and age fields

---
+++ src/day14.java
@@ -0,0 +1,15 @@
+public class day14 {
+    public static void main(String[] args) {
+        
+    }
+     class Person {
+     String name;
+     int age;
+     
+     // Parameterized constructor
+     Person(String name, int age){
+        this.name = name;
+        this.age = age;
+     }
+    }
+}
