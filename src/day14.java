01b33b5b5f1cd2a41904ed6dede0dd6a71c16212
Jun3L <junel041106@gmail.com>
2025-12-29 00:32:45 +0800

    Override toString() in Person

--- src/day14.java
+++ src/day14.java
@@ -1,8 +1,17 @@
 public class day14 {
     public static void main(String[] args) {
+        Person p = new Person("Junel", 19);
+
+        /* hashcode error when not override toString()
+        System.out.println(p);
+         */
+
+        //Override ToString()
+        System.out.println(p);
+
         
     }
-     class Person {
+     static class Person {
       private String name;
      int age;
      
@@ -17,5 +26,15 @@ public class day14 {
         public void setName(String n){
             this.name = n;
         }
+        public int getAge(){
+          return age;
+        }
+        public void setAge(int a){
+            this.age = a;
+        }
+            @Override
+            public String toString(){
+                return "Your name  is " + this.name + " and your age is " + this.age;
+            }
     }
 }
