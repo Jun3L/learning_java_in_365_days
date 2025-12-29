3dfa038ca80245c746ea57dc796ae009f0ea0d19
Jun3L <junel041106@gmail.com>
2025-12-30 01:17:10 +0800

    separate the student and person class

--- src/day14.java
+++ src/day14.java
@@ -11,30 +11,9 @@ public class day14 {
 
         
     }
-     static class Person {
-      private String name;
-     int age;
-     
-     // Parameterized constructor
-     Person(String name, int age){
-        this.name = name;
-        this.age = age;
-     }
-        public String getName(){
-            return name;
-        }
-        public void setName(String n){
-            this.name = n;
-        }
-        public int getAge(){
-          return age;
-        }
-        public void setAge(int a){
-            this.age = a;
-        }
-            @Override
-            public String toString(){
-                return "Your name  is " + this.name + " and your age is " + this.age;
-            }
+    
+            
     }
-}
+   
+    
+
