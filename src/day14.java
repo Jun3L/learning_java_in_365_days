3be5d871d335640c19c2828b9529d069edfab8f7
Jun3L <junel041106@gmail.com>
2025-12-30 21:56:06 +0800

    call introduce() method by using polymorphism

--- src/day14.java
+++ src/day14.java
@@ -1,14 +1,18 @@
 public class day14 {
     public static void main(String[] args) {
-        Person p = new Person("Junel", 19);
+        Person p = new Person("", 0);
+        Person p1 = new Student("", 0);
 
         /* hashcode error when not override toString()
         System.out.println(p);
          */
 
         //Override ToString()
-        System.out.println(p);
+        //System.out.println(p);
 
+        //call introduce() method
+        p1.introduce();
+        p.introduce();
         
     }
     
