9ddcfa6b399d31a5c422497ef421d1b2cff4dcf0
Jun3L <junel041106@gmail.com>
2025-12-30 21:57:45 +0800

    create a introduce() method in Student class

--- src/Student.java
+++ src/Student.java
@@ -2,4 +2,7 @@ public class Student extends Person{
     Student(String name, int age){
         super(name, age);
     }
+    void introduce(){
+        System.out.println("I'm a Student");
+    }
 }
