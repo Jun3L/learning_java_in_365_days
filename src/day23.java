9f77f678e22ef44457683015bad2d97df194c6e7
Jun3L <junel041106@gmail.com>
2026-01-05 13:14:38 +0800

    Add comment to clarify TreeSet behavior in day23.java

--- src/day23.java
+++ src/day23.java
@@ -2,6 +2,8 @@ import java.util.TreeSet;
 public class day23 {
     public static void main(String[] args) {
         TreeSet<Integer> numbers = new TreeSet<Integer>();
+        //TreeSet stores elements in sorted order
+        
         numbers.add(5);
         numbers.add(1);
         numbers.add(3);
