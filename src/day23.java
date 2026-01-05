27e7aac67a6c79120cf8538c9eeda8e0c6a9344c
Jun3L <junel041106@gmail.com>
2026-01-05 13:11:57 +0800

    Use a TreeSet to store sorted unique values

---
+++ src/day23.java
@@ -0,0 +1,14 @@
+import java.util.TreeSet;
+public class day23 {
+    public static void main(String[] args) {
+        TreeSet<Integer> numbers = new TreeSet<Integer>();
+        numbers.add(5);
+        numbers.add(1);
+        numbers.add(3);
+        numbers.add(2);
+        numbers.add(4);
+        for (Integer number : numbers) {
+            System.out.println(number);
+        }
+     }
+}
