89da57d63901e55620bff81ed8705596e3764451
Jun3L <junel041106@gmail.com>
2026-01-20 13:14:36 +0800

    add a comment

--- src/day30.java
+++ src/day30.java
@@ -6,13 +6,16 @@ public class day30 {
         List<String> students = new ArrayList<>();
         students.add("Anna, 20");
         students.add("Josh, 10");
-
+        
+        //write a value in the file
         try {
             Files.write(filepath, students);
             System.out.println("File written successfully");
         } catch (Exception e) {
             System.out.println("Error writing file: " + e.getMessage());
         }
+
+        //read the file
         try {
             List<String> lines = Files.readAllLines(filepath);
                 System.out.println("Reading files content: ");
