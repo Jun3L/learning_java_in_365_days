976168dbd655bd1c72bd051baa68840043eacc19
Jun3L <junel041106@gmail.com>
2026-01-29 15:12:41 +0800

    Use StringBuilder to build a long string efficiently

---
+++ src/day35.java
@@ -0,0 +1,11 @@
+
+public class day35 {
+  public static void main(String[] args) {
+    StringBuilder sb = new StringBuilder();
+    for(int i = 0; i < 5; i++){
+        sb.append("Number: ").append(i).append("\n");
+    }
+    String result = sb.toString();
+    System.out.println(result);
+  }
+}
