b3711e07024647a8c43cbe02d258487742f27b78
Jun3L <junel041106@gmail.com>
2026-02-04 18:30:03 +0800

    Format dates with DateTimeFormatter

--- src/day39.java
+++ src/day39.java
@@ -1,7 +1,10 @@
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 public class day39 {
     public static void main(String[] args) {
         LocalDateTime now = LocalDateTime.now();
-       System.out.println(now);
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+        System.out.println(now.format(formatter));
+       
     }
 }
