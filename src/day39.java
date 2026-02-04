657895abf1a0f8d7fd00bd90c2bf529622d97ca4
Jun3L <junel041106@gmail.com>
2026-02-04 18:28:42 +0800

    Work with dates: print current date/time using java.time.LocalDateTime

---
+++ src/day39.java
@@ -0,0 +1,7 @@
+import java.time.LocalDateTime;
+public class day39 {
+    public static void main(String[] args) {
+        LocalDateTime now = LocalDateTime.now();
+       System.out.println(now);
+    }
+}
