ba05ef7113ef15e04336cd0e2359dc063f98853b
Jun3L <junel041106@gmail.com>
2026-02-26 13:27:23 +0800

    Read JSON from a string using a library (Jackson/Gson)

---
+++ src/Day53/day53.java
@@ -0,0 +1,26 @@
+package Day53;
+import com.google.gson.Gson;
+public class day53 {
+    public static void main(String[] args) {
+        try {
+            // convert json to java object
+        String json = "{\"name\":\"Rico\",\"age\":20,\"course\":\"IT\"}"; //create a json format
+        Gson gson = new Gson(); // gson object
+        Student student = gson.fromJson(json, Student.class);
+
+        System.out.println(student.name);
+        System.out.println(student.age);
+        System.out.println(student.course);
+
+        // Convert Java Back to JSON
+        String newJson = gson.toJson(student);
+        System.out.println(newJson);
+        } catch (Exception e) {
+            System.out.println("JSON Failed");
+            e.printStackTrace();
+        }
+       
+
+    }
+}
+ 
