d05c81d8bf72a5d791dcb54a3bb374c6e6be09c9
Jun3L <junel041106@gmail.com>
2026-02-26 13:26:31 +0800

    modify the structure of student class

---
+++ src/Day50/day50.java
@@ -0,0 +1,22 @@
+package Day50;
+import java.util.*;
+
+public class day50 {
+    public static void main(String[] args) {
+        List<Student> students = Arrays.asList(
+            new Student(2025007445, "Junel"),
+            new Student(2025102785, "Rico"),
+            new Student(2025459871, "Joseph")
+        );
+
+      Optional<Student> result = students.stream()
+                        .filter(s -> s.getName().equals("Rico"))
+                        .findFirst();
+      
+      String studentName = result
+                 .map(Student::getName)
+                 .orElse("Student not found");
+      System.out.println(studentName);
+    }
+}
+
