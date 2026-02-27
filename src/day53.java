f0e8f32c4833989b71a2a5e596af3d2417ee05d4
Jun3L <junel041106@gmail.com>
2026-02-27 11:53:10 +0800

    Use Properties to load config from .properties file

---
+++ src/day53.java
@@ -0,0 +1,29 @@
+import java.io.*;
+import java.util.Properties;
+public class day53 {
+    public static void main(String[] args) {
+        Properties properties = new Properties();
+        try{
+            FileInputStream fils = new FileInputStream("config.properties");
+            properties.load(fils);
+
+            String appName = properties.getProperty("app.name");
+            String appVersion = properties.getProperty("app.version");
+            String dbURL = properties.getProperty("db.url");
+            String dbUser = properties.getProperty("db.user");
+            String dbPassword = properties.getProperty("db.password");
+
+            //Displaying  the setting or info
+
+            System.out.println("App name: " + appName);
+            System.out.println("Version: " + appVersion);
+            System.out.println("DB URL: " + dbURL);
+            System.out.println("DB USER: " + dbUser);
+            System.out.println("DB PASSWORD: " + dbPassword);
+            System.out.println(System.getProperty("user.dir"));
+            fils.close();
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+    }
+}
