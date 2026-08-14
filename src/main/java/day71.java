23664be1f9b91bda02e087a7eae7ce73e4402526
Jun3L <junel041106@gmail.com>
2026-08-14 16:09:46 +0800

    Use assertions with assert to check invariants in development

--- src/main/java/day71.java
+++ src/main/java/day71.java
@@ -1,5 +1,17 @@
+import java.util.Scanner;
+
 public class day71 {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        Scanner s = new Scanner(System.in);
+
+        System.out.print("Enter name : ");
+        String name = s.nextLine();
+        System.out.print("Enter grade : ");
+        int grade = s.nextInt();
+        assert grade >= 0 : "Grade must be non-negative";
+        assert grade <= 100 : "Grade must be less than or equal to 100";
+        System.out.println("Name: " + name);
+        System.out.println("Grade: " + grade);
+        s.close();
     }
 }
