8d094e9128814aac3c3d18943810c10f3f0903e1
Jun3L <junel041106@gmail.com>
2026-09-03 12:48:36 +0800

    Step 3: Upgrade Maven Java Target - Compile: SUCCESS, Tests: passed
    Updated Maven compiler source and target to Java 25. Renamed day45 helper classes to remove the pre-existing default-package collision with day44.
    Security: No security controls changed.

--- src/main/java/day45.java
+++ src/main/java/day45.java
@@ -1,35 +1,39 @@
 public class day45 {
-public static void main(String[] args) throws Exception {
-    Counter counter = new Counter();
+    public static void main(String[] args) throws Exception {
+        CounterDay45 counter = new CounterDay45();
 
-    Worker t1 = new Worker(counter);
-    Worker t2 = new Worker(counter);
-    
-    t1.start();
-    t2.start();
+        WorkerDay45 t1 = new WorkerDay45(counter);
+        WorkerDay45 t2 = new WorkerDay45(counter);
 
-    t1.join();
-    t2.join();
-    
-    System.out.println("Final count: " + counter.count);
-}    
+        t1.start();
+        t2.start();
+
+        t1.join();
+        t2.join();
+
+        System.out.println("Final count: " + counter.count);
+    }
 }
-class Counter{
+
+class CounterDay45 {
     int count = 0;
-    void increment(){
+
+    void increment() {
         count++;
     }
 }
-class Worker extends Thread{
-Counter counter;
-Worker(Counter counter){
-    this.counter = counter;
-}
-public void run(){
-    for (int i = 0; i < 1000; i++) {
-        counter.increment();
-    }
-}
-    
-}
 
+class WorkerDay45 extends Thread {
+    CounterDay45 counter;
+
+    WorkerDay45(CounterDay45 counter) {
+        this.counter = counter;
+    }
+
+    public void run() {
+        for (int i = 0; i < 1000; i++) {
+            counter.increment();
+        }
+    }
+
+}
